429bc37f65a06bbcde4b1279fd0ecc7fbf24bf27
João Gallego <joao.gallego@inovafarma.com.br>
2025-12-21 14:34:45 -0300

    chore: iniciando estrutura da domain e application

---
+++ Services/user-manager/UserManager.Application/UseCases/CreateUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManager.Domain.Entities;
+using UserManager.Domain.Interfaces;
+
+namespace UserManager.Application.UseCases
+{
+    public class CreateUser
+    {
+        private readonly IUserRepository _repository;
+
+        public CreateUser(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ExecuteAsync(string name, string email)
+        {
+            var user = new User(name, email);
+            await _repository.AddAsync(user);
+        }
+
+    }
+}
---
+++ Services/user-manager/UserManager.Domain/Interfaces/IUserManagerRepository.cs
@@ -0,0 +1,12 @@
+﻿using UserManager.Domain.Entities;
+
+namespace UserManager.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(Guid id);
+    Task<User?> GetByIdAsync(Guid id);
+    Task<IEnumerable<User>> GetAllAsync();
+}
