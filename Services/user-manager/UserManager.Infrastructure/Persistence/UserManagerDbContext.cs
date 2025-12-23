8b338eb564d14abdfbb8a51f4d081775a3e90657
João Gallego <joao.gallego@inovafarma.com.br>
2025-12-22 23:06:43 -0300

    feat(api): comeca a configuracao da api e instala swagger

---
+++ Services/user-manager/UserManager.Infrastructure/Persistence/UserManagerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManager.Domain.Entities;
+
+namespace UserManager.Infrastructure.Persistence
+{
+    public class UserManagerDbContext : DbContext
+    {
+        public UserManagerDbContext(DbContextOptions<UserManagerDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
---
+++ Services/user-manager/UserManager.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManager.Domain.Entities;
+using UserManager.Domain.Interfaces;
+using UserManager.Infrastructure.Persistence;
+
+namespace UserManager.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserManagerDbContext _context;
+
+        public UserRepository(UserManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var user = await GetByIdAsync(id);
+            if (user == null) return;
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
