a372bc6d65b5fcc51d24d0ec837350977eb065de
João Gallego <joaogallegopolo@gmail.com>
2025-12-24 18:14:02 -0300

    refactor(user): altera entidade de usuario com simple factory para criacao

--- Services/user-manager/UserManager.Domain/Entities/User.cs
+++ Services/user-manager/UserManager.Domain/Entities/User.cs
@@ -5,22 +5,30 @@ namespace UserManager.Domain.Entities;
 
 public class User : Notifiable<Notification>
 {
+    // Construtor privado (só para EF Core)    
+    private User() { }
+
     public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Email { get; private set; }
     public DateTime CreatedAt { get; private set; }
 
-    public User(string name, string email)
+    public static User Create(string name, string email)
     {
-        Id = Guid.NewGuid();
-        Name = name;
-        Email = email;
-        CreatedAt = DateTime.UtcNow;
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Email = email,
+            CreatedAt = DateTime.UtcNow
+        };
 
-        AddNotifications(new Contract<Notification>()
+        user.AddNotifications(new Contract<Notification>()
             .Requires()
-            .IsNotNullOrWhiteSpace(Name, nameof(Name), "Name is required")
-            .IsEmail(Email, nameof(Email), "Invalid email")
+            .IsNotNullOrWhiteSpace(user.Name, nameof(Name), "Name is required")
+            .IsEmail(user.Email, nameof(Email), "Invalid email")
         );
+
+        return user;
     }
 }
