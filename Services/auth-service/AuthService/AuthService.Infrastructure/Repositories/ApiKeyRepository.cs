71fc45206812485769cfa555e23697c33d488681
João Gallego <joaogallegopolo@gmail.com>
2026-01-07 07:11:20 -0300

    chore: comeca estruturacao da pasrte de infraestrutura do auth

---
+++ Services/auth-service/AuthService/AuthService.Infrastructure/Repositories/ApiKeyRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Infrastructure.Repositories
+{
+    internal class ApiKeyRepository
+    {
+    }
+}
---
+++ Services/auth-service/AuthService/AuthService.Infrastructure/Security/ApiKeyHasher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Infrastructure.Security
+{
+    internal class ApiKeyHasher
+    {
+    }
+}
