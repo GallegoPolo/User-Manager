0aa824c64c2488a6efc1637c26648aee1164688b
João Gallego <joaogallegopolo@gmail.com>
2026-01-07 06:55:47 -0300

    chore: comeca estruturacao das pastas e arquivos da domain

---
+++ Services/auth-service/AuthService/AuthService.Domain/Entities/ApiKey.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Domain.Entities
+{
+    internal class ApiKey
+    {
+    }
+}
---
+++ Services/auth-service/AuthService/AuthService.Domain/Services/ApiKeyDomainService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthService.Domain.Services
+{
+    internal class ApiKeyDomainService
+    {
+    }
+}
