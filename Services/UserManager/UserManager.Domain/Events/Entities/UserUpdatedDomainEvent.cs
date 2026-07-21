6bdc373e662911b62e0bb9677c817c54f832b82c
João Gallego <joaogallegopolo@gmail.com>
2026-07-20 22:45:53 -0300

    improve: adiciona entidades de evento para persistir

---
+++ Services/UserManager/UserManager.Domain/Events/Entities/UserUpdatedDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using UserManager.Domain.Events.Interfaces;
+
+namespace UserManager.Domain.Events.Entities;
+
+public sealed class UserUpdatedDomainEvent(Guid userId, string name, string email) : IDomainEvent
+{
+    public Guid EventId { get; } = Guid.NewGuid();
+    public DateTime OccurredAt { get; } = DateTime.UtcNow;
+    public Guid UserId { get; } = userId;
+    public string Name { get; } = name;
+    public string Email { get; } = email;
+}
